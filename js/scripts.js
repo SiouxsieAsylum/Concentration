e283b0b786dc3ab72ff564c919711dfbd7c86de6
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-10-06 12:08:52 -0400

    basic structure exists now for reset.css

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+let storage = window.localStorage;
+storage.setItem('high-score', score);
+storage.setItem('player', name);
+
+// well already I've set myself two expectations
+// modal for username
+// has an input
+
+window.addEventListener('DOMContentLoaded', function(e){
+  // literally all other code in here
+})
