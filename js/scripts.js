cfe1af5c4a39512880edc132811c5cc9a87496e8
Andrea McKenzie <mckenzie.andrea.m@gmail.com>
2017-10-06 12:43:57 -0400

    word set to and styled with random colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
-let storage = window.localStorage;
-storage.setItem('high-score', score);
-storage.setItem('player', name);
+// let storage = window.localStorage;
+// storage.setItem('high-score', score);
+// storage.setItem('player', name);
 
 // well already I've set myself two expectations
 // modal for username
 // has an input
-
-window.addEventListener('DOMContentLoaded', function(e){
+console.log("I'm here")
+// window.addEventListener('DOMContentLoaded', function(e){
   // literally all other code in here
-})
+  const colors = ["red", "blue", "green", "cyan", "purple", "orange", "yellow", "grey", "black"];
+  const word = document.getElementById("word");
+  const test = document.getElementById("test");
+
+  function randColor(){
+    let randWord = Math.floor(Math.random() * 9 - 0);
+    let randColor = Math.floor(Math.random() * 9 - 0);
+    // console.log(rand);
+    word.innerHTML = colors[randWord];
+    word.style.color = colors[randColor];
+  }
+// })
